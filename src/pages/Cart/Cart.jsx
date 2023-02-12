24338e792514c4887ed157bfa6eb2f318e0b8813
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-02-11 23:20:36 -0800

    cart is fully ready

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link} from 'react-router-dom';
+import { inrementQuantity , decrementQuantity , deleteCartElement } from '../../slice/cartSlice';
 import "./cart.scss";
 
 function Cart(props) {
   const {cart} = useSelector(state => state.cartSlice)
-  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   let totalCost = 0;
   for(let i = 0; i<cart.length ; i++){
@@ -19,6 +20,19 @@ function Cart(props) {
   const buyProducts = () => {
     alert("You bought these products , operators call you soon")
     window.location.reload(true)
+  }
+
+  const handleQuantityIncrement = (id) => {
+    const index = cart.findIndex((item) => item.id === id)
+    dispatch(inrementQuantity(index))
+  }
+  const handleQuantityDecrement = (id) => {
+    const index = cart.findIndex((item) => item.id === id)
+    if(cart[index].quantity>1){
+      dispatch(decrementQuantity(index))
+    }else{
+      dispatch(deleteCartElement(id))
+    }
   }
 
   if(cart.length === 0){
@@ -43,13 +57,13 @@ function Cart(props) {
                 <img src={item.img} alt="" />
                 <h1>{item.name}</h1>
                 <div className="quantity">
-                  <button className='decr'>-</button>
+                  <button className='decr' onClick={() => handleQuantityDecrement(item.id)}>-</button>
                   <h4>{item.quantity}</h4>
-                  <button className='incr'>+</button>
+                  <button className='incr' onClick={() => handleQuantityIncrement(item.id)}>+</button>
                 </div>
                 <div className="cost">
                   <h3>{item.discountCost === "no discount" ? item.cost * item.quantity : item.discountCost * item.quantity} ₽</h3>
-                  <div className="detele">
+                  <div className="detele" onClick={() => dispatch(deleteCartElement(item.id))}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
                   </svg>
@@ -67,6 +81,7 @@ function Cart(props) {
             <h1>{totalCost} ₽</h1>
             <div className="card_footer">
               <button onClick={() => buyProducts()}>Buy Products</button>
+              <button className='purchasing'><Link to="/">Go on purchasing</Link></button>
             </div>
           </div>
         </div>
--- src/slice/cartSlice.js
+++ src/slice/cartSlice.js
@@ -11,8 +11,23 @@ export const cartSlice = createSlice({
     addToCart: (state, action) => {
       state.cart = action.payload;
     },
+    inrementQuantity: (state, action) => {
+      state.cart[action.payload].quantity++;
+    },
+    decrementQuantity: (state, action) => {
+      state.cart[action.payload].quantity--;
+    },
+    deleteCartElement: (state, action) => {
+      const index = state.cart.findIndex((item) => item.id === action.payload);
+      state.cart.splice(index, 1);
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const {
+  addToCart,
+  inrementQuantity,
+  decrementQuantity,
+  deleteCartElement,
+} = cartSlice.actions;
 export default cartSlice.reducer;
